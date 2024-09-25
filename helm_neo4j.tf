a138e788262e1d34bac656def391978af323181d
Prasanth Kommini <prasanth.kommini@snowflake.com>
2024-09-25 16:30:03 -0700

    Fix SG configs.

--- helm_neo4j.tf
+++ helm_neo4j.tf
@@ -22,19 +22,19 @@ resource "helm_release" "neo4j" {
     templatefile(
       "${path.module}/templates/neo4j_values.yaml",
       {
-        module_prefix             = "${var.module_prefix}",
-        neo4j_root_user_password  = "${var.neo4j_root_user_password}",
+        module_prefix            = "${var.module_prefix}",
+        neo4j_root_user_password = "${var.neo4j_root_user_password}",
 
-        neo4j_dns_name          = "${local.neo4j_dns_name}",
-        subdomain_cert_arn      = "${var.subdomain_cert_arn}",
-        tags                    = "environment=${var.env}",
+        neo4j_dns_name     = "${local.neo4j_dns_name}",
+        subdomain_cert_arn = "${var.subdomain_cert_arn}",
+        tags               = "environment=${var.env}",
 
-        allowed_cidr_blocks  = "${join(",", var.allowed_cidr_blocks)}",
-        security_group_ids   = "${join(",", [
-            aws_security_group.security_group_to_allow_outbound_traffic.id,
-            aws_security_group.security_group_to_allow_traffic_to_7687.id,
+        allowed_cidr_blocks = "${join(",", var.allowed_cidr_blocks)}",
+        security_group_ids = "${join(",", [
+          aws_security_group.allow_from_lb_to_eks_clusterf.id,
+          aws_security_group.security_group_to_allow_traffic_to_7687.id,
         ])}",
-        public_subnet_ids    = "${join(",", var.public_subnet_ids)}",
+        public_subnet_ids = "${join(",", var.public_subnet_ids)}",
 
         subdomain_cert_arn   = var.subdomain_cert_arn,
         logs_annotation      = "${local.logs_annotation}",
